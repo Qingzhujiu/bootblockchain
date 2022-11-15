0b90c2e2369a7f3825dad82d4d4de0934b0fc613
Qingzhujiu <Qingzhujiu@qq.com>
2022-11-15 18:54:21 +0800

    2022年11月15日18:53:36 初步增加了登录与注册功能 并且修改了部分bug  但仍存在bug 登录时查询实体类时  部分字段无法录入

--- src/main/java/com/mrli/bootblockchain/BootblockchainApplication.java
+++ src/main/java/com/mrli/bootblockchain/BootblockchainApplication.java
@@ -3,13 +3,21 @@ package com.mrli.bootblockchain;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @MapperScan("com.mrli.bootblockchain.mapper")
 @SpringBootApplication(scanBasePackages = "com.mrli.bootblockchain")
 public class BootblockchainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootblockchainApplication.class, args);
+
+        ConfigurableApplicationContext run = SpringApplication.run(BootblockchainApplication.class, args);
+        /*
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names) {
+                System.out.println(name);
+        }
+         */
     }
 
 }
--- src/test/java/com/mrli/bootblockchain/BootblockchainApplicationTests.java
+++ src/test/java/com/mrli/bootblockchain/BootblockchainApplicationTests.java
@@ -3,11 +3,16 @@ package com.mrli.bootblockchain;
 import com.mrli.bootblockchain.common.R;
 import com.mrli.bootblockchain.controller.UserController;
 import com.mrli.bootblockchain.domain.User;
+import com.mrli.bootblockchain.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BootblockchainApplicationTests {
+    @Autowired
+    private UserController userController;
 
     @Test
     void contextLoads() {
@@ -23,19 +28,25 @@ class BootblockchainApplicationTests {
         user.setSex("男");
         user.setAddress("eaxmple@.com");
         user.setMonad("公安局");
-        user.setPhoneNumber("1584516481");
+        user.setPhone_number("1584516481");
         user.setArea("山东");
         user.setRank("警察");
         user.setEducation("研究生");
-        user.setProfessionalField("计算机");
+        user.setProfessional_field("计算机");
 
-        UserController userController = new UserController();
+
         R<String> stringR = userController.userRegister(user);
         System.out.println(stringR);
-
-
     }
+    @Test
+    void TestLogin() {
+        User user = new User();
+        user.setUsername("小明");
+        user.setPassword("123456");
 
+        R<String> stringR = userController.userLogin(null,user);
+        System.out.println(stringR);
+    }
 
 }
 
